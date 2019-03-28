0e34657304378b8b605d2a3090d58bbcf68e2f1b
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2019-03-28 10:56:50 +0800

    fix bugs

--- install_apk.py
+++ install_apk.py
@@ -8,17 +8,21 @@ from multiprocessing import Pool
 import os
 import sys
 import subprocess
+import time
 
 cur_path = os.getcwd()
 is_shell = True
 
 
+reinstallFlag = 1
+
+
 def run_shell():
-    print ("脚本名：", sys.argv[0])
+    print "脚本名：", sys.argv[0]
     if downloadApkFile(sys.argv[1]) == 0:
-        print ("download success!")
+        print "download success!"
     else:
-        print ("download error")
+        print "download error"
 
     #开始并发安装apk文件
     install_all_apk("~/Desktop/preInstall.apk")
@@ -44,42 +48,74 @@ def get_devicenum():
         line_list = line.split("	")
         if len(line_list) == 2:
             devicenum_list.append(line_list[0])
+    print devicenum_list
     return devicenum_list
 
 # 获取最新的apk文件
 def downloadApkFile(apkAddr):
-
+    print "start download newest apk file"
     abd_proc = subprocess.Popen("curl -w  downloadApk -o ~/Desktop/preInstall.apk -O "+apkAddr, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    print abd_proc
     (__output,__error) = abd_proc.communicate()
     if __output and __output.find("downloadApk") != -1:
+        print "download apk file successfully"
         return 0
+    print "download apk file unsuccessfully"
     return 1
 
-# 安装apk文件
-def install_apk(device_num, apk_file):
-    for i in range(3):
-        adb_proc = subprocess.Popen("adb -s "+device_num+" install -r "+ apk_file, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+# 卸载旧apk文件,安装新apk文件
+def install_apk(device_num, apk_file,old_apk_file):
+
+    # 卸载旧apk文件
+    global reinstallFlag
+    try:
+        print "device %s start uninstalling %s" % (device_num, old_apk_file)
+        adb_proc = subprocess.Popen("adb -s " + device_num + " uninstall " + old_apk_file, stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         (__output, __error) = adb_proc.communicate()
         if __output and __output.find("Success") != -1:
-            print ("device: %s install successfully" % device_num)
+            print "device: %s uninstall %s successfully" % (device_num,old_apk_file)
+    except Exception as e :
+        print "uninstall App error ，reason：doesn't exist such app："+ e.message
+
+    # 安装新apk文件
+    try:
+        print "device %s start installing %s"  % (device_num,apk_file)
+        adb_proc = subprocess.Popen("adb -s "+device_num+" install "+ apk_file, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        (__output, __error) = adb_proc.communicate()
+        if __output and __output.find("Success") != -1:
+            print "device: %s install successfully" % device_num
             return 0
-    print ("device: %s install unsuccessfully" % device_num)
-    return 1
+        print "device: %s install unsuccessfully" % device_num
+        # flag重置为1
+        reinstallFlag = 1
+        return 1
+    except Exception as e:
+        print "error message："+ e.message
+        reinstallFlag +=1
+        if  reinstallFlag < 4:
+            time.sleep(5)
+            print "reinstall app : (%s) times" % reinstallFlag
+            install_apk(device_num,apk_file,old_apk_file)
+        print "can't install apk file , process done :("
 
 
-def install_all_apk(apk_file):
+
+def install_all_apk(apk_file,old_apk_file):
     device_list = get_devicenum()
     p = Pool()
     for device_num in device_list:
-        p.apply_async(install_apk,args=(device_num,apk_file,))
-    print ("Waiting for all subprocess done...")
+        p.apply_async(install_apk,args=(device_num,apk_file,old_apk_file,))
+    print "Waiting for all subprocess done..."
     p.close()
     p.join()
-    print ('All subprocesses done.')
+    print 'All subprocesses done.'
 
 
 if __name__ == '__main__':
-    run_shell()
+    # run_shell()
 
+    #开始并发安装apk文件
+    install_all_apk("~/Desktop/preInstall.apk","com.dianping.v1")
 
 
